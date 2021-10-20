af30c7dac9e5b63971e6e08f4a625008b0fb2dfd
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-10-20 14:33:05 +0300

    start

---
+++ app/src/main/java/org/dark0ghost/android_screen_recoder/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.dark0ghost.android_screen_recoder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
