b6a733a8b791767f8dcf2f8d182ac59c0486a583
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-10-20 23:38:49 +0300

    remove unused valvue param

--- app/src/main/java/org/dark0ghost/android_screen_recorder/state/BaseState.kt
+++ app/src/main/java/org/dark0ghost/android_screen_recorder/state/BaseState.kt
@@ -1,9 +1,9 @@
 package org.dark0ghost.android_screen_recorder.state
 
-enum class BaseState(i: Int) {
-    START(1),
-    READY(1),
-    DONE(2),
-    FILE(3),
-    MIC(4)
+enum class BaseState {
+    START,
+    READY,
+    DONE,
+    FILE,
+    MIC
 }
