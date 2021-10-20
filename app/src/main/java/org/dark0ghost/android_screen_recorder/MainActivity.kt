cee621816ba4224dd99e542018a6a4639edc7067
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-10-20 18:44:57 +0300

    add vosk

--- app/src/main/java/org/dark0ghost/android_screen_recorder/MainActivity.kt
+++ app/src/main/java/org/dark0ghost/android_screen_recorder/MainActivity.kt
@@ -1,10 +1,22 @@
 package org.dark0ghost.android_screen_recorder
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.ColorSpace.Model
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.dark0ghost.android_screen_recorder.utils.Settings.PERMISSIONS_REQUEST_RECORD_AUDIO
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val model: Model? = null
+    private val speechService: SpeechService? = null
+    private val speechStreamService: SpeechStreamService? = null
+    private val resultView: TextView? = null
 
     private fun initModel() {
 
@@ -18,16 +30,47 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun checkPermissionsOrInitialize() {
+        val permissionCheck =
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECORD_AUDIO)
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                PERMISSIONS_REQUEST_RECORD_AUDIO
+            )
+        }
+        initModel()
+    }
+
+    private fun pause(checked: Boolean) {
+        if (speechService != null) {
+            speechService.setPause(checked)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Recognizer initialization is a time-consuming and it involves IO,
+                // so we execute it in async task
+                initModel()
+            } else {
+                finish()
+            }
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        LibVosk.setLogLevel(LogLevel.INFO)
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 }
