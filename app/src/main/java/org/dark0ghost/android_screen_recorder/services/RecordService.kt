f45f40d68f27ce9ed878a36f23080a71979e1729
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-10-24 00:08:26 +0300

    fix: Calling new methods on older versions

--- app/src/main/java/org/dark0ghost/android_screen_recorder/services/RecordService.kt
+++ app/src/main/java/org/dark0ghost/android_screen_recorder/services/RecordService.kt
@@ -110,7 +110,12 @@ open class RecordService: Service() {
         )
         serviceThread.start()
         running = false
-        mediaRecorder = MediaRecorder(this)
+        mediaRecorder =
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S){
+                MediaRecorder(this)
+            } else{
+                MediaRecorder()
+            }
     }
 
     override fun onBind(intent: Intent): IBinder {
