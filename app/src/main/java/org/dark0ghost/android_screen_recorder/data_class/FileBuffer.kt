205c0efa50ec877a0f7227ff4b60f5e1ce4a431e
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-12-17 00:05:34 +0300

    new bug with save subtittle

---
+++ app/src/main/java/org/dark0ghost/android_screen_recorder/data_class/FileBuffer.kt
@@ -0,0 +1,6 @@
+package org.dark0ghost.android_screen_recorder.data_class
+
+import java.io.BufferedWriter
+import java.io.File
+
+data class FileBuffer(val file: File, val bufferedWriter: BufferedWriter)
--- app/src/main/java/org/dark0ghost/android_screen_recorder/manager/SpeechManager.kt
+++ app/src/main/java/org/dark0ghost/android_screen_recorder/manager/SpeechManager.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import org.dark0ghost.android_screen_recorder.data_class.FileBuffer
 import org.dark0ghost.android_screen_recorder.interfaces.GetsDirectory
 import org.dark0ghost.android_screen_recorder.listeners.RListener
 import org.dark0ghost.android_screen_recorder.states.BaseState
@@ -14,6 +15,7 @@ import org.dark0ghost.android_screen_recorder.utils.writeLn
 import org.vosk.Model
 import org.vosk.Recognizer
 import org.vosk.android.SpeechStreamService
+import java.io.BufferedWriter
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -25,11 +27,20 @@ class SpeechManager(private val context: Context, private val model: Model): Get
         .Builder()
         .setCallbackOnFinalResult {
             setUiState(BaseState.DONE)
-            val file = createSubtitleFileOrDefault()
-            file.bufferedWriter().close()
-            Log.d("File/OnFinalResult", file.absoluteFile.toString())
-            Log.d("File/OnFinalResult", file.length().toString())
+            val fileData = createSubtitleFileDataOrDefault()
+            Log.d("File/OnFinalResult", fileData.file.length().toString())
+            Log.d("File/OnFinalResult", "buffer size: ${buffer.size}")
+            if (fileData.file.length() == 0L) {
+                val bufferedWriter = fileData.bufferedWriter
+                buffer.forEach { i ->
+                    bufferedWriter.use {
+                        it.writeLn(i)
+                    }
+                }
+                Log.d("File/OnFinalResult", fileData.file.length().toString())
+            }
             cleanSubtitleFile()
+            fileData.bufferedWriter.close()
             buffer.clear()
             subtitlesCounter = 0
             timer.stop()
@@ -48,8 +59,19 @@ class SpeechManager(private val context: Context, private val model: Model): Get
             $it\n   
             """.trimIndent()
             this@setCallbackOnResult.buffer.add(template)
-            val file = createSubtitleFileOrDefault()
-            file.bufferedWriter().writeLn(template)
+            val fileData = createSubtitleFileDataOrDefault()
+            try {
+                fileData.bufferedWriter.use {
+                    it.writeLn(template)
+                }
+            } catch (e: IOException) {
+                cleanSubtitleFile()
+                val fileBuffer = createSubtitleFileDataOrDefault()
+                fileBuffer.bufferedWriter.use {
+                    it.writeLn(template)
+                }
+                e.printStackTrace()
+            }
             Log.d("File/OnResult", template)
             subtitlesCounter++
             oldTime = timer.nowTime
@@ -63,14 +85,14 @@ class SpeechManager(private val context: Context, private val model: Model): Get
     private var speechStreamService: SpeechStreamService? = null
 
     private var subtitlesCounter: Long = 1L
-    private var subtitleResult: Result<File> = Result.failure(exceptionForResultFile)
+    private var subtitleResult: Result<FileBuffer> = Result.failure(exceptionForResultFile)
     private var oldTime: String = "00:00:00"
 
-    private fun cleanSubtitleFile(){
+    private fun cleanSubtitleFile() {
         subtitleResult = Result.failure(exceptionForResultFile)
     }
 
-    private fun createSubtitleFileOrDefault(): File {
+    private fun createSubtitleFileDataOrDefault(): FileBuffer {
         val textFile = File(
             getsDirectory(),
             "${
@@ -80,11 +102,13 @@ class SpeechManager(private val context: Context, private val model: Model): Get
                 ).format(System.currentTimeMillis())
             }.srt"
         )
+        val bufferedWriter: BufferedWriter = textFile.bufferedWriter()
+        val fileBuffer = FileBuffer(textFile, bufferedWriter)
         if (subtitleResult.isSuccess) {
-            return subtitleResult.getOrDefault(textFile)
+            return subtitleResult.getOrDefault(fileBuffer)
         }
-        subtitleResult = Result.success(textFile)
-        return textFile
+        subtitleResult = Result.success(fileBuffer)
+        return fileBuffer
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -117,13 +141,13 @@ class SpeechManager(private val context: Context, private val model: Model): Get
         }
     }
 
-    fun stop(){
+    fun stop() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             stopMicrophone()
         }
     }
 
-    fun close(){
+    fun close() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             stopMicrophone()
         }
@@ -135,7 +159,8 @@ class SpeechManager(private val context: Context, private val model: Model): Get
     }
 
     override fun getsDirectory(): String {
-        val rootDir = "${context.getExternalFilesDir("media")!!.absolutePath}/${Settings.MediaRecordSettings.NAME_DIR_SUBTITLE}/"
+        val rootDir =
+            "${context.getExternalFilesDir("media")!!.absolutePath}/${Settings.MediaRecordSettings.NAME_DIR_SUBTITLE}/"
         val file = File(rootDir)
         if (!file.exists()) {
             Log.e(
