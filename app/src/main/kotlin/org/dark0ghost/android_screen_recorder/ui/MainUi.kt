5ac99ac845ef6ef55ad4a403fbf56add531a2eba
dark0ghost <dark0ghostworkmail@seznam.cz>
2022-01-28 22:55:30 +0300

    fix gradle: kotlin version downgrade 1.6.10 -> 1.5.31, added file in utils: compose_utils

--- app/src/main/kotlin/org/dark0ghost/android_screen_recorder/ui/MainUi.kt
+++ app/src/main/kotlin/org/dark0ghost/android_screen_recorder/ui/MainUi.kt
@@ -1,10 +1,26 @@
 package org.dark0ghost.android_screen_recorder.ui
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.dark0ghost.android_screen_recorder.utils.ImageWithBackground
+import org.dark0ghost.android_screen_recorder.R
 
 @Preview
 @Composable
 fun MainUI() {
-
+    ImageWithBackground(
+        painter = painterResource(id = R.drawable.ic_notification_custom),
+        backgroundDrawableResId = R.drawable.dark_night,
+        contentDescription = "",
+        modifier = Modifier
+            .height(160.dp)
+            .width(160.dp)
+            .padding(32.dp),
+    )
 }
